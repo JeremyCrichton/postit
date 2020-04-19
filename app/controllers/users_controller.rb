55d309fd3db27f02ca9e4f37f99d3f6a0f48e26f
Jeremy Crichton <22128243+JeremyCrichton@users.noreply.github.com>
2020-04-19 11:09:21 -0700

    add edit and show for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  
+def show
+  
+end
+
   # Register a new users at /register
   def new
     @user = User.new
@@ -13,6 +19,23 @@ class UsersController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Your profile was upddated."
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
   def user_params
