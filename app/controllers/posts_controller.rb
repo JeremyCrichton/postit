3e35b92137f887fa9b265a7e04eb6e2b5eeb54c2
Jeremy Crichton <22128243+JeremyCrichton@users.noreply.github.com>
2020-04-19 19:20:18 -0700

    complete voting functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,9 +39,15 @@ class PostsController < ApplicationController
   end
 
   def vote
-    Vote.create(voteable: @post, creator: current_user, vote: params[:vote])
-    flash[:notice] = 'Your vote was counted.'
-    redirect_to :back # go back to where you just were when you voted
+    vote = Vote.create(voteable: @post, creator: current_user, vote: params[:vote])
+    
+    if vote.valid?
+      flash[:notice] = 'Your vote was counted.'
+    else
+      flash[:error] = 'You can only vote on a post once.'
+    end
+    
+    redirect_back(fallback_location: root_path)
   end
 
   private
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,7 @@ class Vote < ActiveRecord::Base
   # class_name and foreign_key are necessary because "creator" deviates from the default of user
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
   belongs_to :voteable, polymorphic: true
+
+  # Same user can't create a vote for the same object
+  validates_uniqueness_of :creator, scope: [:voteable_id, :voteable_type]
 end
